591e9b646bfb7720a9cecc40706951f0f6cf4f74
mweise91 <mweise@students.kennesaw.edu>
2019-01-18 09:32:40 -0500

    Cleaned up code
    Still need to redo comments

--- perceptron.java
+++ perceptron.java
@@ -6,7 +6,7 @@ import java.text.*;
 import java.math.*;
 import java.util.List;
 public class perceptron {
-    //declare static variables
+   
     static int maxIterations = 16;
     static double learningRate = 0.1;
     static int numberOfInstances = 16;
@@ -18,13 +18,7 @@ public class perceptron {
         int p2[] = {-1,1,-1,1,1,-1,1,1,-1,-1,1,1,1,-1,-1,-1};
         int p3[] = {-1,1,-1,1,1,1,-1,-1,1,1,-1,1,-1,1,-1,-1};
         int p4[] = {-1,-1,1,1,-1,1,1,1,-1,1,-1,1,-1,-1,-1,1};
-        //System.out.println(outputs.length + " " + p1.length + " " + p2.length + " " + p3.length +" " + p4.length);
 
-        /*for (int i = 0; i <16; i++){
-            System.out.println("[" + p1[i] + "|" + p2[i] + "]");
-            System.out.println("[" + p3[i] + "|" + p4[i] + "]");
-            System.out.println();
-        }*/
         double weights[] = new double[5];
         Random random = new Random();
         weights[0] = random.nextDouble() *2 -1;
@@ -32,7 +26,7 @@ public class perceptron {
         weights[2] = random.nextDouble() *2 -1;
         weights[3] = random.nextDouble() *2 -1;
         weights[4] = random.nextDouble() *2 -1;
-        //System.out.println(Arrays.toString(weights));
+  
         double localError, globalError;
         int iteration = 0;
         int output;
@@ -40,22 +34,22 @@ public class perceptron {
             iteration++;
             globalError = 0;
             System.out.println("Iteration "+iteration);
-            //loop through all instances
+           
             for (int p = 0; p < numberOfInstances; p++) {
-                //calculate predicted class
+                
                 output = CalculateOutput(theta, weights, p1[p], p2[p], p3[p], p4[p]);
                 System.out.println("Given: " + outputs[p] + "\tPrediction: " + output);
-                //difference between predicted and actual
+       
                 localError = outputs[p] - output;
                 weights[0] += learningRate * localError * p1[p];
                 weights[1] += learningRate * localError * p2[p];
                 weights[2] += learningRate * localError * p3[p];
                 weights[3] += learningRate * localError * p4[p];
                 weights[4] += learningRate * localError * 1;
-                //error value for all instances
+   
                 globalError += (localError * localError);
             }
-            //System.out.println("Iteration "+iteration);
+     
         } while ((globalError != 0) && (iteration <= maxIterations));
         boolean testMode = true;
         Scanner sc = new Scanner(System.in);
@@ -96,23 +90,17 @@ public class perceptron {
 
     }
 
-    //calculate output
     static int CalculateOutput(int t, double[] w, int p1, int p2, int p3, int p4){
         double sum = p1 * w[0] + p2 * w[1] + p3 * w[2] + p4 * w[3] + 1 * w[4];
         return (sum >= t) ? 1 :-1;
     }
 
-    //generate dark image
     static void test0(double[] w){
         int p1[] = {-1,-1,1,-1,-1};
         int p2[] = {1,-1,-1,-1,-1};
         int p3[] = {-1,1,-1,-1,-1};
         int p4[] = {-1,-1,-1,1,-1};
-        /*for (int i = 0; i <5; i++){
-            System.out.println("[" + p1[i] + "|" + p2[i] + "]");
-            System.out.println("[" + p3[i] + "|" + p4[i] + "]");
-            System.out.println();
-        }*/
+        
         Random random = new Random();
         int p = random.nextInt(5);
         int prediction = CalculateOutput(theta,w, p1[p], p2[p], p3[p], p4[p]);
@@ -123,17 +111,13 @@ public class perceptron {
 
     }
 
-    //generate light image
+
     static void test1(double[] w){
         int p1[] = {-1,1,-1,1,1,1,-1,1,-1,1,1};
         int p2[] = {1,-1,1,1,-1,1,1,-1,-1,1,1};
         int p3[] = {1,-1,1,1,1,-1,-1,1,1,-1,1};
         int p4[] = {-1,1,1,-1,1,1,1,-1,1,-1,1};
-        /*for (int i = 0; i <11; i++){
-            System.out.println("[" + p1[i] + "|" + p2[i] + "]");
-            System.out.println("[" + p3[i] + "|" + p4[i] + "]");
-            System.out.println();
-        }*/
+
         Random random = new Random();
         int p = random.nextInt(11);
         int prediction = CalculateOutput(theta,w, p1[p], p2[p], p3[p], p4[p]);
@@ -143,7 +127,6 @@ public class perceptron {
                 "The machine predicted: " + prediction);
     }
 
-    //random image
     static void test2(double[] w){
         int p1[] = {-1,-1,1,-1,1,1,1,-1,1,-1,1,1,-1,-1,1,-1};
         int p2[] = {-1,1,-1,1,1,-1,1,1,-1,-1,1,1,1,-1,-1,-1};
@@ -159,7 +142,6 @@ public class perceptron {
 
     }
 
-    //manually submit
     static void test3(double[] w){
         System.out.println("Enter -1 for a black pixel and 1 for a white pixel:");
         Scanner sc = new Scanner(System.in);
